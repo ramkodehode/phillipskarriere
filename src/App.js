27ecc81cf058c625ee626a495220dfd2df15f1ed
ramkodehode <ramkodehode@gmail.com>
2023-02-23 13:39:42 +0100

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Sucess from "./pages/Success";
+import { NavTitle } from "./styles/Navbar";
+import PhillipLogo from "../src/img/PhillipLogo.png";
+import KomIgjen from "./pages/Komigjen";
+import { ImageLogo } from "./styles/Styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <ImageLogo src={PhillipLogo} alt="logo" />
+      <NavTitle>Phillips Karriere</NavTitle>
+      <Home />
+      <Sucess />
+      <KomIgjen />
     </div>
   );
 }
